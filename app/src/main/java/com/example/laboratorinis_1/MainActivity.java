cd9c39e42e784cfd7d7fc5984fb28103baab262e
Agk158 <agne.kazlauskaite@knf.stud.vu.lt>
2021-04-18 16:12:08 +0300

    color change button

--- app/src/main/java/com/example/laboratorinis_1/MainActivity.java
+++ app/src/main/java/com/example/laboratorinis_1/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.laboratorinis_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvSecondary;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +21,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void OnBtnClick(View view) {
         this.tvSecondary.setText("We got a lot to offer!");
-    }
-
-    public void OnBtnClick2(View view) {
-        this.tvSecondary.setTextColor(Color.GREEN);
     }
 }
